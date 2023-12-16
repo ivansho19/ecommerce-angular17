95886540e19c599878f213ec49dff946c99ec33b
ivan manrique <ivanmanrique1993@gmail.com>
2023-12-16 10:12:44 -0500

    create new component product

---
+++ src/app/domains/products/components/product/product.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../../../shared/components/models/product.model';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  // @Input({required: true}) img : string = '';
+  // @Input({required: true}) price: number = 0;
+  // @Input({required: true}) title: string = '';
+  @Input({required: true}) product!: Product;
+
+  @Output() addToCart = new EventEmitter();
+
+  addToCartHandler(){
+    console.log('click from child');
+    this.addToCart.emit('Hola este es un msg desde el hijo ' + this.product.title);
+  }
+
+}
