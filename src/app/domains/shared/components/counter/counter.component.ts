f744df236566b13a3e50928efa9c55f73656aef9
ivan manrique <ivanmanrique1993@gmail.com>
2023-12-16 10:15:47 -0500

    create new component counter

---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,70 @@
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  @Input({required: true}) duration = 0;
+  @Input({required: true}) message = '';
+  counter = signal(0);
+  counterRef: number | undefined;
+
+  constructor(){
+    // no async 
+    // before render
+    // One 
+    console.log('constructor');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // before and during render
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+    console.log(changes);
+    const duration = changes['duration'];
+    if(duration && duration.currentValue !== duration.previousValue){
+      this.doSomething();
+    }
+
+  }
+
+  ngOnInit(){
+    // after render 
+    // una vez
+    // async, then, subs
+    console.log('ngOnInit');
+    console.log('-'.repeat(10));
+    console.log('duration =>', this.duration);
+    console.log('message =>', this.message);
+    this.counterRef = window.setInterval(()=> {
+      console.log('run interval');
+      this.counter.update(statePrev => statePrev + 1);
+    }, 1000)
+
+  }
+
+  ngAfterViewInit(){
+    //after render
+    // hijos ya fueron pintados
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy(){
+    // when the component is destroy
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(10));
+    window.clearInterval(this.counterRef);
+  }
+
+  doSomething(){
+    console.log('change duration');
+  }
+
+
+}
