6ac649c703f6a92f3ed0e18b833ec7cafa877558
ivan manrique <ivanmanrique1993@gmail.com>
2023-12-24 01:26:16 -0500

    adds integration with Endpoint product services

--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -11,7 +11,15 @@ export class ProductService {
 
   constructor() { }
 
-  getProducts(){
-    return this.http.get<Product[]>(this.URL_PRODUCTS);
+  getProducts(categorory_id?: string){
+    const url = new URL(this.URL_PRODUCTS);
+    if(categorory_id){
+      url.searchParams.set('categoryId', categorory_id);
+    }
+    return this.http.get<Product[]>(url.toString());
+  }
+
+  getProductOne(id: string){
+    return this.http.get<Product>(this.URL_PRODUCTS+"/"+id);
   }
 }
